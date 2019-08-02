83a9ec94c725c086683c5ac270d2727abfdedbcf
JoseGonzalez6227 <51135938+JoseGonzalez6227@users.noreply.github.com>
2019-08-02 13:44:53 -0500

    Add files via upload

---
+++ src/CountSheep.java
@@ -0,0 +1,11 @@
+//Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+class CountSheep {
+    public static String countingSheep(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+            stringBuilder.append(i).append(" sheep...");
+        }
+        return stringBuilder.toString();
+    }
+}
